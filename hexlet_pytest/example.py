70e53435db88b4ede2aa6cc86110c736c5015123
Mariia Bilinska <mariiaklochko@gmail.com>
2021-11-16 14:09:26 +0000

    Doctest for reverse() function

--- hexlet_pytest/example.py
+++ hexlet_pytest/example.py
@@ -1,2 +1,17 @@
 def reverse(s):
+    """Reverse a string.
+
+    >>> reverse('')
+    ''
+    >>> reverse('Hexlet')
+    'telxeH'
+    >>> reverse('Barbara Streisand')
+    'dnasiertS arabraB'
+    """
     return s[::-1]
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
